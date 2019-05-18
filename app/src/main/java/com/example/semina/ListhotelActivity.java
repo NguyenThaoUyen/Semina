b048dcc505b94d2277c685ed2bc2dd3622d10e4f
NguyenThaoUyen <nguyenthaouyen0997@gmail.com>
2019-05-19 01:50:29 +0700

    mới commit nè:):)

---
+++ app/src/main/java/com/example/semina/ListhotelActivity.java
@@ -0,0 +1,13 @@
+package com.example.semina;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListhotelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listhotel);
+    }
+}
