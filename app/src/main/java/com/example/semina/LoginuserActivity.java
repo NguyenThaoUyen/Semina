9002d93d67be9610a9679364bc47833fca64a7e3
NguyenThaoUyen <nguyenthaouyen0997@gmail.com>
2019-05-20 01:17:46 +0700

    mới commit nè 20/5

---
+++ app/src/main/java/com/example/semina/LoginuserActivity.java
@@ -0,0 +1,13 @@
+package com.example.semina;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginuserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loginuser);
+    }
+}
--- app/src/main/java/com/example/semina/MainActivity.java
+++ app/src/main/java/com/example/semina/MainActivity.java
@@ -2,8 +2,26 @@ package com.example.semina;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+                String TAG ="FIREBASE";
+                EditText ed_user, ed_password;
+                Button btn_login , btn_register;
+                TextView logn , register;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +31,27 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+        //create fire base
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+        // get data
+        DatabaseReference myRef = database.getReference("user");
+        //truy suất và lắng nghe sự thay đổi dữ liệu
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
     }
 
 
